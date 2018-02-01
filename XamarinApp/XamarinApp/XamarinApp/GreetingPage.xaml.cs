8ed01962b14f394a0dcd1764b046a6a30ea0b95e
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-01 15:22:55 -0500

    Control device differences with code behind

--- XamarinApp/XamarinApp/XamarinApp/GreetingPage.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/GreetingPage.xaml.cs
@@ -9,15 +9,37 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class GreetingPage : ContentPage
-	{
-		public GreetingPage ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetingPage : ContentPage
+    {
+        public GreetingPage()
+        {
+            InitializeComponent();
 
             slider.Value = 0.5;
+
+            if (Device.OS == TargetPlatform.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+            else if (Device.OS == TargetPlatform.Android)
+                Padding = new Thickness(10, 20, 0, 0);
+
+            Device.OnPlatform(
+               iOS: new Thickness(0, 20, 0, 0),
+               Android: new Thickness(10, 20, 0, 0),
+               WinPhone: new Thickness(30, 20, 0, 0)
+                );
+
+            //executing code based on OS
+            Device.OnPlatform(
+                iOS: () =>
+                {
+                    Padding = new Thickness(0, 20, 0, 0);
+                },
+                Android: () =>
+                {
+                    Padding = new Thickness(0, 20, 0, 0);
+                });
         }
-         
+
     }
 }
