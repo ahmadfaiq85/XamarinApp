a159e117d4cd81033d1690a4aeeb8cdc9ba09dd8
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-04 01:11:39 -0500

    Absolute page in code

--- XamarinApp/XamarinApp/XamarinApp/App.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/App.xaml.cs
@@ -13,7 +13,7 @@ namespace XamarinApp
 		{
 			InitializeComponent();
 
-			MainPage = new XamarinApp.AbsoluteLayoutExercise();
+			MainPage = new XamarinApp.AbsolutePage();
 		}
 
 		protected override void OnStart ()
