fbda0cf5df1afeab5e6df943caafa2a2c9d53a83
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-11 18:49:32 -0500

    Activity Indicator

--- XamarinApp/XamarinApp/XamarinApp/Images.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/Images.xaml.cs
@@ -18,14 +18,14 @@ namespace XamarinApp
 
             //Image source using code
             var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/2/") };
-            imageSource.CachingEnabled = true;
+            imageSource.CachingEnabled = false;
             imageSource.CacheValidity = TimeSpan.FromHours(12);
 
             image.Source = imageSource;
 
             //Aspect ratios
-            image.Aspect = Aspect.AspectFill;
-            //image.Aspect = Aspect.AspectFit;
+            //image.Aspect = Aspect.AspectFill;
+            image.Aspect = Aspect.AspectFit;
             //image.Aspect = Aspect.Fill;
         }
 	}
