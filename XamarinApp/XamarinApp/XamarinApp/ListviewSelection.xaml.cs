54d5a396269429f25b5c1defd83cdab4b1c5e311
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-12 15:05:20 -0500

    Listview Context Actions

--- XamarinApp/XamarinApp/XamarinApp/ListviewSelection.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/ListviewSelection.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,31 +13,45 @@ namespace XamarinApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListviewSelection : ContentPage
 	{
+        private ObservableCollection<Contact> _contacts;
 		public ListviewSelection ()
 		{
 			InitializeComponent ();
 
-            listview.ItemsSource = new List<Contact>
+            _contacts = new ObservableCollection<Contact>
             {
                 new Contact {Name = "Faiq", ImageUrl = "http://lorempixel.com/100/100/people/3"},
                 new Contact {Name = "Taha", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hi, how is it going?"}
             };
+
+            listview.ItemsSource = _contacts;
         }
 
         void Item_Selected(object sender, SelectedItemChangedEventArgs e)
         {
-            //listview.SelectedItem = null;
-
-            var contact = e.SelectedItem as Contact;
-            DisplayAlert("Selected", contact.Name, "OK");
+            //listview.SelectedItem = null; 
+            //var contact = e.SelectedItem as Contact;
+            //DisplayAlert("Selected", contact.Name, "OK");
         }
 
         void Item_Tapped(object sender,  ItemTappedEventArgs e)
         {
-            var contact = e.Item as Contact; 
-            DisplayAlert("Selected", contact.Name, "OK"); 
+            //var contact = e.Item as Contact; 
+            //DisplayAlert("Selected", contact.Name, "OK"); 
         }
 
+        void Call_Clicked (object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var contact = menuItem.CommandParameter as Contact;
 
+            DisplayAlert("Call", contact.Name, "OK");
+        }
+
+        void Delete_Clicked (object sender, EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            _contacts.Remove(contact);
+        }
 	}
 }
