ba5c98ead46ec60815d673896f2b9045a4580ff2
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-12 15:11:44 -0500

    Listview Pull down to refresh

--- XamarinApp/XamarinApp/XamarinApp/ListviewSelection.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/ListviewSelection.xaml.cs
@@ -10,21 +10,26 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ListviewSelection : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListviewSelection : ContentPage
+    {
         private ObservableCollection<Contact> _contacts;
-		public ListviewSelection ()
-		{
-			InitializeComponent ();
+        public ListviewSelection()
+        {
+            InitializeComponent();
 
-            _contacts = new ObservableCollection<Contact>
+            _contacts = GetContacts();
+
+            listview.ItemsSource = _contacts;
+        }
+
+        ObservableCollection<Contact> GetContacts()
+        {
+            return new ObservableCollection<Contact>
             {
                 new Contact {Name = "Faiq", ImageUrl = "http://lorempixel.com/100/100/people/3"},
                 new Contact {Name = "Taha", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hi, how is it going?"}
             };
-
-            listview.ItemsSource = _contacts;
         }
 
         void Item_Selected(object sender, SelectedItemChangedEventArgs e)
@@ -34,13 +39,13 @@ namespace XamarinApp
             //DisplayAlert("Selected", contact.Name, "OK");
         }
 
-        void Item_Tapped(object sender,  ItemTappedEventArgs e)
+        void Item_Tapped(object sender, ItemTappedEventArgs e)
         {
             //var contact = e.Item as Contact; 
             //DisplayAlert("Selected", contact.Name, "OK"); 
         }
 
-        void Call_Clicked (object sender, EventArgs e)
+        void Call_Clicked(object sender, EventArgs e)
         {
             var menuItem = sender as MenuItem;
             var contact = menuItem.CommandParameter as Contact;
@@ -48,10 +53,18 @@ namespace XamarinApp
             DisplayAlert("Call", contact.Name, "OK");
         }
 
-        void Delete_Clicked (object sender, EventArgs e)
+        void Delete_Clicked(object sender, EventArgs e)
         {
             var contact = (sender as MenuItem).CommandParameter as Contact;
             _contacts.Remove(contact);
         }
-	}
+
+        void List_Refreshing(object sender, EventArgs e)
+        {
+            listview.ItemsSource = GetContacts();
+
+            //listview.IsRefreshing = false;
+            listview.EndRefresh();
+        }
+    }
 }
