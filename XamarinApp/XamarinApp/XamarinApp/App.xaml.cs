708617f1f1d1b31ac7647b4963eef2e66a430ec6
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-02-11 21:01:16 -0500

    Images exercise photogallery

--- XamarinApp/XamarinApp/XamarinApp/App.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/App.xaml.cs
@@ -13,7 +13,7 @@ namespace XamarinApp
 		{
 			InitializeComponent();
 
-			MainPage = new XamarinApp.Images();
+			MainPage = new XamarinApp.ImagesExercise();
 		}
 
 		protected override void OnStart ()
---
+++ XamarinApp/XamarinApp/XamarinApp/ImagesExercise.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImagesExercise : ContentPage
+	{
+        private int _currentImage = 1;
+		public ImagesExercise ()
+		{
+			InitializeComponent ();
+
+            Load_Image(); 
+        }
+
+        void Load_Image()
+        {
+            var imageSource = new UriImageSource { Uri = new Uri(string.Format("http://lorempixel.com/1920/1080/sports/{0}/", _currentImage)) };
+            
+            image.Source = imageSource;
+        }
+
+        void Previous_Clicked(object sender, EventArgs e)
+        {
+            _currentImage--;
+            Load_Image();
+        }
+
+        void Next_Clicked(object sender, EventArgs e)
+        {
+            _currentImage++;
+            Load_Image();
+        }
+	}
+}
